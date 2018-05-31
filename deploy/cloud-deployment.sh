7ee07d22629e8c234ef3709db9407ff17e083e72
Lluis <lluis@weareadaptive.com>
2018-05-31 18:52:28 +0200

    [Deployment] Added full deployment with helm.

--- deploy/cloud-deployment.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-kubectl create -f service.yaml --namespace="insights"
-sleep 10
-kubectl create -f client-nginx-cm.yaml --namespace="insights"
-sleep 10
-kubectl create -f deployment.yaml --namespace="insights"
