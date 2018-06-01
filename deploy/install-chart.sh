519d532a9357898fc755050d707598ed9cfd723d
Lluis <lluis@weareadaptive.com>
2018-06-01 16:46:53 +0200

    [Deployment] Added README, edited descriptions, added ignores.

--- deploy/install-chart.sh
+++ deploy/install-chart.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+namespace="${1:-insights}"
+
 helm install insights \
     --name="insights" \
 	--version=0.0.1   \
 	--set sslSecret.certcrt="$(gsutil cat gs://adaptivecluster-secrets-ssl/cert.crt)" \
 	--set sslSecret.certkey="$(gsutil cat gs://adaptivecluster-secrets-ssl/cert.key)" \
-    --values="insights/values-demo.yaml" --namespace="insights"
+    --values="insights/values-demo.yaml" --namespace="$namespace"
