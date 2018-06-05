457f33c717fa3580e4e0d8ffca6ecb91c104b312
unknown <tcdev00@gmail.com>
2018-06-05 14:34:40 +0200

    [Deployment] Added build scripts and edits in Dockerfiles. Install chart script contains prevention of execution if parameters are not filled.

--- deploy/install-chart.sh
+++ deploy/install-chart.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+if [ $# -lt 5 ]; then
+    echo "" >&2
+	echo "Usage: [CLIENT_TAG] [SERVER_TAG] [NAMESPACE] [LOAD_BALANCER_IP] [DOMAIN_NAME]" >&2
+	echo "" >&2
+	echo "    [CLIENT_TAG] Tag from docker image that will be used by kubernetes to fetch the artifact from GCP." >&2
+	echo "    [SERVER_TAG] Tag from docker image that will be used by kubernetes to fetch the artifact from GCP." >&2
+	echo "    [NAMESPACE] Kubernetes cluster namespace that will be used as release name." >&2
+	echo "    [LOAD_BALANCER_IP] IP of the load balancer assigned to this release. Find in GCP." >&2
+	echo "    [DOMAIN_NAME] Name of the domain binded to the load balancer IP." >&2
+	echo "" >&2
+	echo "CAUTION: Load balancer IP and domain name should be tied previously." >&2
+	echo "" >&2
+    exit 1
+fi
+
 client_tag="${1}"
 server_tag="${2}"
 namespace="${3}"
