df6c926c939c207102dfce0f4058d43876f30ca9
Lluis <lluis@weareadaptive.com>
2018-06-18 11:45:00 +0200

    [OpenFin] OpenFin deployment improvements.

--- deploy/install-chart.sh
+++ deploy/install-chart.sh
@@ -9,6 +9,7 @@ if [ $# -lt 5 ]; then
 	echo "    [NAMESPACE] Kubernetes cluster namespace that will be used as release name." >&2
 	echo "    [LOAD_BALANCER_IP] IP of the load balancer assigned to this release. Find in GCP." >&2
 	echo "    [DOMAIN_NAME] Name of the domain binded to the load balancer IP." >&2
+	echo "    [ENVIRONMENT] Environment where the application will be deployed. Example format: (DEV), (TEST)..." >&2
 	echo "" >&2
 	echo "CAUTION: Load balancer IP and domain name should be tied previously." >&2
 	echo "" >&2
@@ -20,14 +21,16 @@ server_tag="${2}"
 namespace="${3}"
 loadBalancerIP="${4}"
 domain_name="${5}"
+environment="${6}"
 
 helm upgrade "$namespace" insights \
   --install \
-	--version=0.0.1   \
+	--version=0.0.1 \
 	--set deploymentClient.image.tag="$client_tag" \
 	--set deploymentServer.image.tag="$server_tag" \
 	--set sslSecret.certcrt="$(gsutil cat gs://adaptivecluster-secrets-ssl/cert.crt)" \
 	--set sslSecret.certkey="$(gsutil cat gs://adaptivecluster-secrets-ssl/cert.key)" \
 	--set serviceClient.loadBalancerIP="$loadBalancerIP" \
 	--set deploymentClient.nginxConf.domainName="$domain_name" \
+	--set openFin.environment="$environment" \
   --namespace="$namespace"
